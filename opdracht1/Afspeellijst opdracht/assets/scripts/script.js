d0ff5ab8e9fb52e28a16b37cd3f02dba91c58495
Rowin Schmidt <rowinschmidt@gmail.com>
2021-02-09 14:18:58 +0100

    Version 1

---
+++ opdracht1/Afspeellijst opdracht/assets/scripts/script.js
@@ -0,0 +1,40 @@
+// DRAG SORTING ITEMS - WITH AUTO REARRANGE
+// https://codepen.io/fitri/full/oWovYj/
+
+function enableDragSort(listClass) {
+    const sortableLists = document.getElementsByClassName(listClass);
+    Array.prototype.map.call(sortableLists, (list) => {enableDragList(list)});
+}
+
+function enableDragList(list) {
+    Array.prototype.map.call(list.children, (item) => {enableDragItem(item)});
+}
+
+function enableDragItem(item) {
+    item.setAttribute('draggable', true)
+    item.ondrag = handleDrag;
+    item.ondragend = handleDrop;
+}
+
+function handleDrag(item) {
+    const selectedItem = item.target,
+        list = selectedItem.parentNode,
+        x = event.clientX,
+        y = event.clientY;
+
+    selectedItem.classList.add('drag-sort-active');
+    document.querySelector("main section > ul").classList.add("unfocus");
+    let swapItem = document.elementFromPoint(x, y) === null ? selectedItem : document.elementFromPoint(x, y);
+
+    if (list === swapItem.parentNode) {
+        swapItem = swapItem !== selectedItem.nextSibling ? swapItem : swapItem.nextSibling;
+        list.insertBefore(selectedItem, swapItem);
+    }
+}
+
+function handleDrop(item) {
+    item.target.classList.remove('drag-sort-active');
+    document.querySelector("main section > ul").classList.remove("unfocus");
+}
+
+(()=> {enableDragSort('drag-sort-enable')})();
