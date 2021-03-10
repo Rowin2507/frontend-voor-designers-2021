c2914ce14efecd66566677f3f6a4ebc5d3e7c740
Rowin Schmidt <rowinschmidt@gmail.com>
2021-03-10 13:15:26 +0100

    final

--- opdracht2/v1/assets/scripts/script.js
+++ opdracht2/v1/assets/scripts/script.js
@@ -1,5 +1,5 @@
 var body = document.querySelector('body');
-
+let root = document.documentElement;
 
 // SETTINGS DROPDOWN --------------------------------------------------------------------------------------
 var settingsOverlay = document.querySelector('main section:nth-of-type(1) > ul > li > ul');
@@ -18,7 +18,7 @@ function settingsOverlayToggle() {
 // OPEN SETTINGS DROPDOWN (SPACEBAR)
 // https://developer.mozilla.org/en-US/docs/Web/API/Document/keydown_event
 body.addEventListener("keydown", event => {
-    if (event.isComposing || event.keyCode === 32) {
+    if (event.isComposing || event.key === ' ') {
         settingsOverlayToggle();
         event.preventDefault();
         return;
@@ -33,22 +33,43 @@ toggleDarkmode.addEventListener("click", toggleDarkmodeAppearance);
 
 function toggleDarkmodeAppearance() {
     if (toggleDarkmode.checked == true){
-        document.querySelector('body').classList.add('darkmode');
+        body.classList.add('darkmode');
     } else {
-        document.querySelector('body').classList.remove('darkmode');
+        body.classList.remove('darkmode');
     }
 }
 
 // DARKMODE TOGGLE SLIDER (D-key)
 // https://developer.mozilla.org/en-US/docs/Web/API/Document/keydown_event
 body.addEventListener("keydown", event => {
-    if (event.isComposing || event.keyCode === 68) {
+    if (event.isComposing || event.key === 'd') {
         if (toggleDarkmode.checked == true){
             toggleDarkmode.checked = false;
         } else {
             toggleDarkmode.checked = true;
         }
         toggleDarkmodeAppearance();
+        return;
+    }
+});
+
+
+
+// COLUMNS TOGGLE SLIDER --------------------------------------------------------------------------------------
+var toggleColumns = document.querySelector('main section:nth-of-type(1) > ul > li > ul > li:nth-of-type(2) > ul');
+toggleColumns.addEventListener("click", toggleColumnsAmount);
+var toggleColumnsValue = document.querySelector('main section:nth-of-type(1) > ul > li > ul > li:nth-of-type(2) > ul li:nth-of-type(2)');
+
+function toggleColumnsAmount() {
+    body.classList.toggle('less-columns');
+    toggleColumnsValue.textContent == '3' ? toggleColumnsValue.textContent = '4' : toggleColumnsValue.textContent = '3'; 
+}
+
+// COLUMNS TOGGLE SLIDER (K-key)
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/keydown_event
+body.addEventListener("keydown", event => {
+    if (event.isComposing || event.key === 'k') {
+        toggleColumnsAmount();
         return;
     }
 });
@@ -64,18 +85,49 @@ function textSizeSlider() {
 
     // CHANGE CSS VARIABLE
     // https://css-tricks.com/updating-a-css-variable-with-javascript/
-    let root = document.documentElement;
-    root.style.setProperty('--font-size', textSizeRangeValue + "px");
+    root.style.setProperty('--font-size', textSizeRangeValue + 'px');
 }
 
-// // MOVE SLIDER LEFT (ARROW LEFT)
-// // https://developer.mozilla.org/en-US/docs/Web/API/Document/keydown_event
-// body.addEventListener("keydown", event => {
-//     if (event.isComposing || event.keyCode === 37) {
-//         textSizeRange.stepDown(2);
-//         return;
-//     } else if (event.isComposing || event.keyCode === 39) {
-//         textSizeRange.stepUp(2);
-//         return;
-//     }
-// });
+// MOVE SLIDER LEFT & RIGHT (ARROW LEFT & RIGHT)
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/keydown_event
+body.addEventListener("keydown", event => {
+    if (event.isComposing || event.keyCode === 37) {
+        textSizeRange.stepDown(1);
+        textSizeSlider();
+        return;
+    } else if (event.isComposing || event.keyCode === 39) {
+        textSizeRange.stepUp(1);
+        textSizeSlider();
+        return;
+    }
+});
+
+
+
+// COMPACT TOGGLE SLIDER --------------------------------------------------------------------------------------
+var toggleCompact = document.querySelector('.toggle-slider.compact input');
+toggleCompact.addEventListener("click", toggleCompactAppearance);
+
+function toggleCompactAppearance() {
+    if (toggleCompact.checked == true){
+        body.classList.add('compact');
+        root.style.setProperty('--image-height', '75px');
+    } else {
+        body.classList.remove('compact');
+        root.style.setProperty('--image-height', '215px');
+    }
+}
+
+// COMPACT TOGGLE SLIDER (C-key)
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/keydown_event
+body.addEventListener("keydown", event => {
+    if (event.isComposing || event.key === 'c') {
+        if (toggleCompact.checked == true){
+            toggleCompact.checked = false;
+        } else {
+            toggleCompact.checked = true;
+        }
+        toggleCompactAppearance();
+        return;
+    }
+});
